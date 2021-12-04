5468087b22e40eb1c215dec0a99494876a199dfd
ellagwithington <ella.withington@runningtide.com>
2021-12-04 16:11:17 -0500

    added server communication

--- mainBoard.java
+++ mainBoard.java
@@ -16,15 +16,29 @@ import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.layout.*;
 import javafx.stage.Stage;
+import javafx.scene.layout.VBox;
+
+import javafx.scene.layout.HBox;
 import javafx.scene.text.Font;
 import javafx.scene.control.*;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.util.*;
 public class mainBoard extends Application {
 
      
     private int row = 5;
     private int column = 6;
     private final int FONT_SIZE = 20;
-     
+    private Label columnNum = new Label("Enter your column number:");
+    private TextField columnField = new TextField();
+    private Button button = new Button("Send");
+    private Label errorMessage = new Label();
+    final StackPane stack = new StackPane();
+    final GridPane gridPane = new GridPane();
     public static void main(String[] args) {
         launch(args);
     }
@@ -32,27 +46,33 @@ public class mainBoard extends Application {
     public void start(Stage primaryStage) {
          
         final BorderPane root = new BorderPane();
-        final GridPane gridpane = new GridPane();
+        
+        
         primaryStage.setTitle("Connect Four");
         
          
         Scene scene = new Scene(root, 800, 600, true);
         scene.setFill(Color.WHITE);
         
-        gridpane.setAlignment(Pos.CENTER);
+        gridPane.setAlignment(Pos.CENTER);
          
         
-        createBoard(gridpane);
-       // dropPiece();
-        root.setCenter(gridpane);
+        createBoard(gridPane);
+        dropPiece(1);
+        root.setCenter(gridPane);
         primaryStage.setScene(scene);
         
         primaryStage.show();
     }
      
 
-    private void createBoard(final GridPane gridpane){
+    private void createBoard(final GridPane gridPane){
         final Circle gamePiece = new Circle(40);
+       
+        gridPane.add(columnNum, 6, 5);
+        gridPane.add(columnField, 6, 6);
+        gridPane.add(button, 6, 7);
+        button.setOnAction(e -> send());
         gamePiece.setFill(Color.RED);
         for(int r = 1;r < row +1; r++){
             for(int c = 0; c < column; c++){
@@ -61,7 +81,9 @@ public class mainBoard extends Application {
 //number the columns
             Label userLabel2 = new Label(String.valueOf(c));
 	        userLabel2.setFont(new Font(FONT_SIZE));
-	        gridpane.add(userLabel2, c, 0);
+            gridPane.add(userLabel2, c, 0);
+             errorMessage.setTextFill(Color.RED);
+             gridPane.add(errorMessage, 5, 6);
             Rectangle rect = new Rectangle(100,100);
             Circle circ = new Circle(47);
             circ.centerXProperty().set(50);
@@ -71,11 +93,11 @@ public class mainBoard extends Application {
             cell.setStroke(Color.BLUE);
             
           
-            StackPane stack = new StackPane();
+           
              
             stack.getChildren().addAll(gamePiece, cell);
              
-            gridpane.add(stack, c, r); 
+            gridPane.add(stack, c, r); 
              
               
             }
@@ -86,8 +108,43 @@ public class mainBoard extends Application {
     }
     
     private void dropPiece(int column){
-        final Circle diskPreview = new Circle(40);
-        diskPreview.setFill(Color.RED);
-        
+        final Circle gamePiece = new Circle(40);
+        gamePiece.setFill(Color.RED);
+        stack.getChildren().addAll(gamePiece);
+             
+            gridPane.add(stack, column, 5); 
     }
+     public void send(){
+        try {
+            Socket connection = SceneManager.getSocket(); // Server socket
+			//Socket connection = SceneManager.getSocket(); // Server socket
+	    	PrintWriter outgoing;   // Stream for sending data.
+			outgoing = new PrintWriter( connection.getOutputStream() );
+			
+			String move = columnField.getText();
+			outgoing.println(move);
+			outgoing.flush();  // Make sure the data is actually sent!
+            System.out.println("Sent move"); // For debugging
+//            outgoing.close();
+
+            BufferedReader incoming = new BufferedReader( 
+                    new InputStreamReader(connection.getInputStream()) );
+            System.out.println("Waiting for result...");
+            String reply = incoming.readLine();
+//            incoming.close();
+            if (reply.equals("SUCESS")) {
+            	errorMessage.setText("");
+            	dropPiece(Integer.parseInt(move));
+            }
+            else if (reply.equals("FAIL")) {
+            	errorMessage.setText("Please choose another move");
+            }
+            else
+            	errorMessage.setText(reply);
+		}
+        catch (Exception e) {
+            System.out.println("Error:  " + e);
+        }
+	}
+
 } 
