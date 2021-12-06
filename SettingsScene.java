7a4dfa01facaac1f41380926588fee74eef6ee8a
eilerb <37630910+eilerb@users.noreply.github.com>
2021-12-05 22:48:39 -0500

    Update SettingsScene.java
    Added read and write capabilities to SettingsScene. Made the buttons functional.

--- SettingsScene.java
+++ SettingsScene.java
@@ -2,61 +2,36 @@ import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
 import javafx.scene.layout.HBox;
-import javafx.scene.layout.GridPane; 
+import javafx.scene.layout.GridPane;
 import javafx.geometry.Insets;
 import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
 import javafx.scene.text.Font;
 import javafx.stage.Stage;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
+import java.io.*;
 
-public class SettingsScene{
-    private static Stage stage;
+public class SettingsScene {
     private Scene scene;
     protected VBox root = new VBox();
-	private Label you = new Label("Color of your Disk");
+    private Label you = new Label("Color of your Disk");
     private TextField color1;
-	private Label opponent = new Label("Color of opponent's Disk");
-	private TextField color2;
-	private Button saveButton = new Button("Save");
-	private Button cancelButton = new Button("Cancel");
-	private Label errorMessage = new Label();
+    private Label opponent = new Label("Color of opponent's Disk");
+    private TextField color2;
+    private Button saveButton = new Button("Save");
+    private Button cancelButton = new Button("Cancel");
+    private Label errorMessage = new Label();
 
 
-	public SettingsScene() {
+    public SettingsScene() {
         Label message = new Label("Settings");
         message.setFont(new Font(40));
         root.getChildren().addAll(message);
         root.setAlignment(Pos.TOP_CENTER);
         scene = new Scene(root, 450, 250);
 
-        //Reading XML file
-            try {
-                BufferedReader reader = new BufferedReader(new FileReader("Settings.xml")); // For reading the file.
-                String line = reader.readLine();
-
-                // Read lines until end of file.
-                while (line != null) {
-                    // Check if line equals color1.
-                    if (line.trim().equals("<color1>")) {
-                        color1 = new TextField(reader.readLine().trim());
-                    }
-
-                    // Check if line equals username.
-                    else if (line.trim().equals("<color2>")) {
-                        color2 = new TextField(reader.readLine().trim());
-                    }
-                    line = reader.readLine();
-                }
-
-                // Close reader.
-                reader.close();
-
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
+        // Read settings from file
+        readSettingsFile();
 
         //Creating Grid Pane 
         GridPane gridPane = new GridPane();
@@ -66,7 +41,7 @@ public class SettingsScene{
         gridPane.setHgap(5);
         gridPane.add(you, 0, 0);
         gridPane.add(color1, 1, 0);
-        gridPane.add(opponent, 0,1);
+        gridPane.add(opponent, 0, 1);
         gridPane.add(color2, 1, 1);
         HBox buttonBox = new HBox();
         buttonBox.getChildren().addAll(saveButton, cancelButton);
@@ -75,13 +50,67 @@ public class SettingsScene{
         gridPane.add(errorMessage, 1, 3);
         gridPane.setAlignment(Pos.TOP_CENTER);
         root.getChildren().addAll(gridPane);
-	}
+        saveButton.setOnAction(e -> writeSettingsFile());
+        cancelButton.setOnAction(e -> SceneManager.setStartScene());
+    }
 
     public Scene getScene() {
         return scene;
     }
 
     public void setStage(Stage stage) {
-        SettingsScene.stage = stage;
+    }
+
+    private void readSettingsFile() {
+        try {
+            BufferedReader reader = new BufferedReader(new FileReader("Settings.xml")); // For reading the file.
+            String line = reader.readLine();
+
+            // Read lines until end of file.
+            while (line != null) {
+                // Check if line equals color1.
+                if (line.trim().equals("<color1>")) {
+                    color1 = new TextField(reader.readLine().trim());
+                }
+
+                // Check if line equals username.
+                else if (line.trim().equals("<color2>")) {
+                    color2 = new TextField(reader.readLine().trim());
+                    break;
+                }
+                line = reader.readLine();
+            }
+
+            // Close reader.
+            reader.close();
+
+        } catch (Exception e) {
+            errorMessage.setText("Error reading settings file.");
+            color1 = new TextField("");
+            color2 = new TextField("");
+        }
+    }
+
+    private void writeSettingsFile() {
+        try {
+            PrintWriter file = new PrintWriter("Settings.xml");
+            file.println("<?xml version=\"1.0\"?>");
+            file.println("<Settings>");
+            file.println("\t<color1>");
+            file.println("\t\t" + color1.getText());
+            file.println("\t</color1>");
+            file.println("\t<color2>");
+            file.println("\t\t" + color2.getText());
+            file.println("\t</color2>");
+            file.println("</Settings>");
+
+            file.flush();
+            file.close();
+
+            // Set startScene
+            SceneManager.setStartScene();
+        } catch (Exception e) {
+            errorMessage.setText("Error writing settings file.");
+        }
     }
 }
